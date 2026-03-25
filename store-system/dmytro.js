c5e2d8c3d5084d6b385f928d92705c0554f4a79e
Dmyto-admin <dmytromoroz2023@gmail.com>
2026-03-25 16:53:58 +0200

    Create dmytro.js

---
+++ store-system/dmytro.js
@@ -0,0 +1,56 @@
+import { db } from "./firebase-config.js";
+import { onSnapshot, collection } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
+
+function loadUserInvoices(userEmail) {
+  const container = document.querySelector(".wrapper-payments .profile-info");
+
+  const invoicesRef = collection(db, "users", userEmail, "invoices");
+
+  onSnapshot(invoicesRef, (snapshot) => {
+
+    container.innerHTML = ""; // clear old
+
+    if (snapshot.empty) {
+      container.innerHTML = `
+        <img src="no-payment-yet.png">
+        <p class="no-payment-yet-text">No payments yet</p>
+      `;
+      return;
+    }
+
+    snapshot.forEach(docSnap => {
+      const data = docSnap.data();
+
+      const block = document.createElement("div");
+      block.classList.add("invoice-block");
+
+      block.innerHTML = `
+        <div class="invoice-line"></div>
+
+        <p><strong>Invoice ID:</strong> ${data.invoiceId}</p>
+        <p><strong>Order ID:</strong> ${data.orderId}</p>
+        <p><strong>Product:</strong> ${data.productName}</p>
+        <p><strong>Total:</strong> ${data.finalPrice}</p>
+        <p><strong>Date:</strong> ${data.date} ${data.time}</p>
+
+        <button class="download-btn">
+          Download PDF
+        </button>
+
+        <div class="invoice-line"></div>
+      `;
+
+      const btn = block.querySelector(".download-btn");
+
+      btn.addEventListener("click", () => {
+        const link = document.createElement("a");
+        link.href = data.pdf;
+        link.download = "Invoice_" + data.invoiceId + ".pdf";
+        link.click();
+      });
+
+      container.appendChild(block);
+    });
+
+  });
+             }

f804ca00b0dbd798223944848b7f32e572293db4
Dmyto-admin <dmytromoroz2023@gmail.com>
2026-03-28 11:10:40 +0100

    Update firebase-config.js

--- store-system/firebase-config.js
+++ store-system/firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAs2sXfHY3IRrss_TLiVBLmuKF7daWFFGA",
@@ -12,5 +13,14 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+signInAnonymously(auth)
+  .then((userCredential) => {
+    console.log("✅ Anonymous user:", userCredential.user.uid);
+  })
+  .catch((error) => {
+    console.error("❌ Auth error:", error);
+  });
+
+export { db, auth };

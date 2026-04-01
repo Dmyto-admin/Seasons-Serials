d9e50f79b4546c2ca8380bb73ae6a321f7d033aa
Dmyto-admin <dmytromoroz2023@gmail.com>
2026-04-01 23:25:06 +0200

    Update dmytro.js

--- store-system/dmytro.js
+++ store-system/dmytro.js
@@ -32,7 +32,7 @@ function loadUserInvoices(userEmail) {
 
     // 🔥 SORT BY DATE
     invoicesArray.sort((a, b) => {
-      return new Date(a.date) - new Date(b.date);
+      return new Date(b.date) - new Date(a.date);
     });
 
     // 🔥 RENDER

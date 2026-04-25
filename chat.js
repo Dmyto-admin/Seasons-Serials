c11e15c6edfa4876843115dc3ec5b115fd8be31a
Dmyto-admin <dmytromoroz2023@gmail.com>
2026-04-25 13:14:39 +0200

    Remove unused import for semanticSearchStrict

--- chat.js
+++ chat.js
@@ -1,7 +1,7 @@
 import { db } from "./store-system/firebase-config.js";
 import { addDoc, collection, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 import { doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
-import { semanticSearchStrict } from "./semantic-search.js";
+
 
 
 /* CONTENTS:
